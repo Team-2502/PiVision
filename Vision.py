7c30865917c593dfe4a5d1b8630f5060bb19397c
Ritik Mishra <ritik.mishra314@gmail.com>
2017-02-08 18:44:30 -0600

    no need for HSV conversion, write to image file in order to find HSV ranges

--- Vision.py
+++ Vision.py
@@ -3,43 +3,11 @@ import numpy as np
 import Util
 import EdgeTest as edge
 
-#on = Util.getCurrentFrameMultiplier(0.5, 0.5)
-#off = Util.getCurrentFrameMultiplier(0.5, 0.5)
-
-#sub = cv2.subtract(on, off);
-#hsv = cv2.cvtColor(sub, cv2.COLOR_BGR2HSV)
-
-# Make 7 windows
+# Make 4 windows
 Util.nw('0') # frame
 Util.nw('1') # hsv
-# Util.nw('2')
-# Util.nw('3')
-# Util.nw('4')
-# Util.nw('5')
-# Util.nw('6')
 Util.nw('7') # frame edges
 Util.nw('8') # filtered hsv
-
-def null(_):
-    return
-
-# Place a bunch of trackbars onto window 6
-# cv2.createTrackbar('0', '6', 0, 255, null)
-# cv2.createTrackbar('1', '6', 0, 255, null)
-# cv2.createTrackbar('2', '6', 0, 255, null)
-#
-# cv2.createTrackbar('3', '6', 255, 255, null)
-# cv2.createTrackbar('4', '6', 255, 255, null)
-# cv2.createTrackbar('5', '6', 255, 255, null)
-
-# Make a 3 by 3 matrix filled with ones
-kernel = np.ones((3, 3), np.uint8)
-
-#lowerBound = np.array([50, 100, 20])
-#upperBound = np.array([255, 255, 255])
-
-frame = cv2.imread('VL0_G.png')
-frame = cv2.bitwise_not(frame)
 
 # define our boundary for red in BGR
 # THE COLORS ARE IN [BLUE, GREEN, RED]
@@ -49,51 +17,22 @@ boundary = [
 ]
 
 while True:
-#    on = Util.getCurrentFrameMultiplier(0.5, 0.5)
-#    off = Util.getCurrentFrameMultiplier(0.5, 0.5)
     frame = Util.getCurrentFrameMultiplier(0.5, 0.5)
-    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    lowerBound = np.array([cv2.getTrackbarPos('0', '6'), cv2.getTrackbarPos('1', '6'), cv2.getTrackbarPos('2', '6')])
-    upperBound = np.array([cv2.getTrackbarPos('3', '6'), cv2.getTrackbarPos('4', '6'), cv2.getTrackbarPos('5', '6')])
-
-    on = Util.getCurrentFrameMultiplier(0.25, 0.25)
-    off = Util.getCurrentFrameMultiplier(0.25, 0.25)
-
-    #sub = cv2.subtract(on, off)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     lower = np.array(boundary[0][0], dtype = "uint8")
     upper = np.array(boundary[0][1], dtype = "uint8")
 
-    mask_hsv = cv2.inRange(hsv, lower, upper)
-    filtered_hsv = cv2.bitwise_and(hsv, hsv, mask = mask_hsv)
-    frame_edges = cv2.Canny(filtered_hsv, 290, 100)
-
-    # mask = cv2.inRange(hsv, lowerBound, upperBound)
-    # output = cv2.bitwise_and(frame, frame, mask=mask)
-
-    # threshold = Util.modifyThreshold(output, 10)
-    # outputThreshold = cv2.cvtColor(threshold, cv2.COLOR_GRAY2BGR)
-    # erode = cv2.erode(outputThreshold, kernel, iterations=1)
-    # erode2 = cv2.erode(threshold, kernel, iterations=5)
-
-    # image, contours, hierarchy = cv2.findContours(erode2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#    cv2.imshow('Vid Cap', threshold)
-#    cv2.imshow('Vid Cap', outputThreshold)
+    frame_mask = cv2.inRange(frame, lower, upper)
+    filtered_frame = cv2.bitwise_and(frame, frame, mask = frame_mask)
+    frame_edges = cv2.Canny(filtered_frame, 290, 100)
 
 
     cv2.imshow('0', frame)
-    cv2.imshow('1', hsv)
-    # cv2.imshow('2', mask)
-    # cv2.imshow('3', threshold)
-    # cv2.imshow('4', erode)
-    # cv2.imshow('5', erode2)
     cv2.imshow('7', frame_edges)
     cv2.imshow('8', filtered_hsv)
 
-    cv2.imwrite("hsv.jpg", hsv)
-    
+    cv2.imwrite("frame.jpg", frame)
+
     # print("Midpoint: " + str(edge.middle(frame_edges)))
     # print(" :" + str(edge.objdimensions(frame_edges)))
 
@@ -101,5 +40,3 @@ while True:
         break
 
 Util.cleanUp()
-#lowerBound = np.array([50, 100, 20])
-#upperBound = np.array([255, 255, 255])
