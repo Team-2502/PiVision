438a7ab6248b6a15bae96d810adbb731c5595a92
Ritik Mishra <ritik.mishra314@gmail.com>
2017-08-05 00:17:28 -0500

    height stuff works probably

--- Vision.py
+++ Vision.py
@@ -4,6 +4,7 @@ from Utils import PiCamera
 import EdgeTest as edge
 from networktables import NetworkTables
 from Utils import nones
+import Utils
 from EdgeTest import overlay
 import time
 
@@ -11,18 +12,19 @@ import time
 # THE COLORS ARE IN [BLUE, GREEN, RED]
 # DO NOT FORGET!
 boundary = [
-    ([120, 133, 0],[255, 255, 255])
+    ([120, 133, 0], [255, 255, 255])
 ]
 
-lower = np.array(boundary[0][0], dtype = "uint8")
-upper = np.array(boundary[0][1], dtype = "uint8")
+lower = np.array(boundary[0][0], dtype="uint8")
+upper = np.array(boundary[0][1], dtype="uint8")
 
 cams = [PiCamera(0)]
 ##### How many cameras are we looking a
 camnum = 1
 #####
-#for x in range(camnum):
+# for x in range(camnum):
 #    cams.append(PiCamera(x))
+
 
 
 # Initalize variables
@@ -35,10 +37,12 @@ frame_edges = base
 # Connect to RoboRIO via networktable
 NetworkTables.initialize(server='roborio-2502-frc.local')
 visionTable = NetworkTables.getTable('PiVision')
- 
+
 oldtime = time.time() * 1000
 sequence = 0
 starttime = oldtime
+height = (None, None)
+offset = (None, None)
 while True:
     # open eyes
     for x, cam in enumerate(cams):
@@ -48,27 +52,25 @@ while True:
     for camnum, frame in enumerate(frames):
         # ignore irrelevant colors
         frame_mask[camnum] = cv2.inRange(frame, lower, upper)
-        filtered_frame[camnum] = cv2.bitwise_and(frame, frame, mask = frame_mask[camnum])
-        frame_edges[camnum] = cv2.cvtColor(filtered_frame[camnum], cv2.COLOR_BGR2GRAY)
-    
-        offset = edge.middle(frame_edges[camnum], True)
-        
+        offset = edge.middle(frame_mask[camnum], True)
+        height = edge.height(frame_mask[camnum], True)
+
         # print(offset[0])
         # cv2.imshow(str(camnum), frames[camnum])
 
         # print("Midpoint: " + str(edge.middle(frame_edges[camnum])))
     currenttime = time.time() * 1000
-    for x, frame_edge in enumerate(frame_edges):
+    for x, frame_edge in enumerate(frame_mask):
         visionTable.putNumber("offset", float(offset[0]))
-        visionTable.putNumber("sequence", float(sequence)) 
+        visionTable.putNumber("sequence", float(sequence))
         visionTable.putNumber("time", float(currenttime - starttime))
         visionTable.putNumber("timeelapsed", float(currenttime - oldtime))
+        visionTable.putNumber("height", float(height[0]))
         oldtime = currenttime
-        #pass
+        # pass
     sequence += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-Util.cleanUp()
- 
+Utils.cleanUp()
