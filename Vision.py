1dfb5bb327c1f561a1c27579bf739e03d81f7bf1
Ritik Mishra <ritik.mishra314@gmail.com>
2018-08-30 15:37:38 -0500

    Significantly speed up code. Fixes #5 as far as I am concerned.

--- Vision.py
+++ Vision.py
@@ -3,20 +3,10 @@ import numpy as np
 from Utils import PiCamera
 import EdgeTest as edge
 from networktables import NetworkTables
-from Utils import nones
-import Utils
-from EdgeTest import overlay
 import time
 import sys
 
-enableLiveFeed = False
-enableTerminalLogging = False
-if len(sys.argv) > 1:
-    enableLiveFeed = True
-
-if len(sys.argv) > 2:
-    enableTerminalLogging = True
-
+MULTIPLIER = 0.5
 
 # define our boundary for red in BGR
 # THE COLORS ARE IN [BLUE, GREEN, RED]
@@ -28,76 +18,65 @@ boundary = [
 lower = np.array(boundary[0][0], dtype="uint8")
 upper = np.array(boundary[0][1], dtype="uint8")
 
-cams = [PiCamera(0)]
-##### How many cameras are we looking a
-camnum = 1
-#####
-# for x in range(camnum):
-#    cams.append(PiCamera(x))
-
-
-
-# Initalize variables
-base = nones(camnum)
-frames = base
-frame_mask = base
-filtered_frame = base
-frame_edges = base
+starttime = None
+begintime = time.time()
+endtime = None
 
 # Connect to RoboRIO via networktable
 NetworkTables.initialize(server='roborio-2502-frc.local')
 visionTable = NetworkTables.getTable('PiVision')
 
-oldtime = time.time() * 1000
+oldtime = time.time()
 sequence = 0
-starttime = oldtime/1000
-height = (None, None)
-offset = (None, None)
+
+offset = None
+
+currentFPS = None
+
+averageFPS = None
+
+camera = PiCamera(0)
 
 def getFrame():
-    multiplier = 0.5
-    return np.rot90(cam.getCurrentFrameMultiplier(0.125, 0.125), 3)
+    return np.rot90(camera.getCurrentFrameMultiplier(MULTIPLIER, MULTIPLIER))
+
+
+frame = getFrame()
+frame_mask = np.zeros(frame.shape)
+
 while True:
-    if sequence % 1000 == 1:
-        print("Still running good")
-    # open eyes
-    for x, cam in enumerate(cams):
-        if sequence == 0:
-            try:
-                frames[x] = getFrame()
-            except AttributeError:
-                print("ERROR: Another vision process is running.\nTo fix this error, run\n$ killall python")
-                sys.exit()
-        frames[x] = getFrame()
+    starttime = time.time()
 
-    # think about what i am seeing
-    for camnum, frame in enumerate(frames):
-        # ignore irrelevant colors
-        frame_mask[camnum] = cv2.inRange(frame, lower, upper)
-        offset = edge.middle(frame_mask[camnum], False) # Biggest drain on FPS
-        height = edge.height(frame_mask[camnum], False) # Also probably a big drain
+    frame = getFrame()
 
-        # print(offset[0])
-        if enableLiveFeed:
-            cv2.imshow(str(camnum), frames[camnum])
-	if enableTerminalLogging:
-            print("Height:" , height)
-            print("Offset:", offset)
-            print("###")
+    cv2.inRange(frame, lower, upper, frame_mask)
 
-        # print("Midpoint: " + str(edge.middle(frame_edges[camnum])))
-    currenttime = time.time() * 1000
-    for x, frame_edge in enumerate(frame_mask):
-        visionTable.putNumber("robot_offset", float(offset))
-        visionTable.putNumber("fps", float(sequence/(time.time() - starttime)))
-        visionTable.putNumber("height", float(height))
-        visionTable.putNumber("offset", float(offset))
-        # visionTable.putNumber("fps", float(sequence / (time.time() - starttime)))
-        oldtime = currenttime
-        # pass
+    offset = edge.middle(frame_mask)
+
+    endtime = time.time()
+
     sequence += 1
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    currentFPS = (1 / (endtime - starttime))
+    averageFPS = (sequence / (endtime - begintime))
+
+    if len(sys.argv) > 1:
+        print("offset:", offset)
+        print("fps:", currentFPS)
+        print("avgfps:", averageFPS)
+        print()
+
+    if len(sys.argv) > 2:
+        cv2.imshow("vision", frame_mask)
+
+    if len(sys.argv) > 3:
+        cv2.imshow("camera", frame)
+
+    visionTable.putNumber("robot_offset", float(offset))
+    visionTable.putNumber("fps", currentFPS)
+    visionTable.putNumber("avgfps", averageFPS)
+    visionTable.putNumber("offset", float(offset))
+
+
 
 Utils.cleanUp()
